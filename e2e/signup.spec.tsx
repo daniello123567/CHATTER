cb46a7d7fd2189d7d4cceb2f535de3ab5fcc3d8d
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-08-12 02:35:34 +0100

    signUptestdone

--- e2e/signup.spec.tsx
+++ e2e/signup.spec.tsx
@@ -1,5 +1,28 @@
 import {test,expect} from 'playwright/test'
-
+const Url = 'http://localhost:3000'
 test('Sign Up Auth', async ({page})=>{
-  page.goto('http://localhost:3000/signUp')
-})
+  await page.goto(`${Url}/signUp`);
+  await expect(page).toHaveURL(`${Url}/signUp`);
+  await expect(page).toHaveTitle(/sign up to chatter/);
+});
+test('Take user to Choose Categories they like for personlized content After sign Up',async ({page})=>{
+  const getMeThisElement = (element:string)=>{
+     return page.locator(element)
+  }
+  const randomEmailsoTestdoesntCrashWithEmailALreadyTaken = `Daniel${Date.now()}-+clerk_test@example.com`;
+  const randomUserNamesoTestdoesntCrashWithUserNameALreadyTaken = `Daniel${Date.now()}`;
+
+  await page.goto(`${Url}/signUp`);
+  const firstName = getMeThisElement('.cl-formFieldInput__firstName');
+  const LastName = getMeThisElement('.cl-formFieldInput__lastName');
+  const userName = getMeThisElement('.cl-input__username');
+  const emailAdress = getMeThisElement('.cl-formFieldInput__emailAddress');
+  const password = getMeThisElement('.cl-formFieldInput__password ');
+  await firstName.fill('Daniel')
+  await LastName.fill('Code')
+  await userName.fill(randomUserNamesoTestdoesntCrashWithUserNameALreadyTaken)
+  await emailAdress.fill(randomEmailsoTestdoesntCrashWithEmailALreadyTaken)
+  await password.fill('Testuser101')
+  await page.locator('.cl-formButtonPrimary').click();
+  await expect(page).toHaveURL(`${Url}/personalization`)
+});
