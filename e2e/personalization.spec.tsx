ab26fbbd3f0a094acc3cc9c7daf5b0cba9e980fd
Daniel Adetaiwo Eniola <131781327+daniello123567@users.noreply.github.com>
2024-08-21 18:21:28 +0100

    Update personalization.spec.tsx

--- e2e/personalization.spec.tsx
+++ e2e/personalization.spec.tsx
@@ -2,12 +2,12 @@ import { test , expect} from 'playwright/test'
 const Url = "https://chatter-six-kappa.vercel.app/personalization"
 test('Personalizaton', async ({page})=>{
   await page.goto(Url);
-  expect(page).toHaveTitle(/Choose Your Fun On Chatter/);
+  await expect(page).toHaveTitle(/Choose Your Fun On Chatter/);
 })
 test('Confirm Categories', async ({page})=>{
   await page.goto(Url);
   const cat = page.locator('.cat');
   const CodingCategory = page.getByRole('button',{name:/coding/i})
   await CodingCategory.click();
-  expect(cat).toHaveText("Choosen Categories: Coding")
+  await expect(cat).toHaveText("Choosen Categories: Coding")
 })
